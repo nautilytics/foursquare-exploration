cb08383d63324dffcd252d63bce5f701c453eb4c
Chris <chris.lanoue@premise.com>
2021-11-23 14:20:02 -0500

    refactor to get cursor

--- caching/api-query.js
+++ caching/api-query.js
@@ -5,7 +5,7 @@ const rp = require('request-promise');
 
 const cachingQuery = (module.exports = (qs, ttl = 0) => {
     return new Promise(async (resolve, reject) => {
-        let err, results;
+        let results;
 
         const hash = crypto.createHash('sha1')
             .update(JSON.stringify(qs))
@@ -25,26 +25,49 @@ const cachingQuery = (module.exports = (qs, ttl = 0) => {
 function _execute(qs, ttl) {
     return new Promise(async (resolve, reject) => {
         let err;
-        let result
+        let response;
+        let link;
+        let results = [];
         const hash = crypto.createHash('sha1')
             .update(JSON.stringify(qs))
             .digest('hex');
-        [err, result] = await to(rp({
-            uri: 'https://api.foursquare.com/v3/places/search',
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': process.env.FOURSQUARE_API_KEY
-            },
-            qs,
-            json: true
-        }));
-        if (err) {
-            return reject(err)
-        }
+        do {
+            if (!link) {
+                [err, response] = await to(rp({
+                    uri: 'https://api.foursquare.com/v3/places/search',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Authorization': process.env.FOURSQUARE_API_KEY
+                    },
+                    qs,
+                    json: true,
+                    resolveWithFullResponse: true
+                }));
+            } else {
+                [err, response] = await to(rp({
+                    uri: link,
+                    headers: {
+                        'Accept': 'application/json',
+                        'Authorization': process.env.FOURSQUARE_API_KEY
+                    },
+                    json: true,
+                    resolveWithFullResponse: true
+                }))
+            }
+            if (err) {
+                return reject(err)
+            }
 
-        redisClient.setCache('four_square', hash, ttl, result, (err, data) => {
-            if (err || !data) return reject('Error getting redis cache');
-            return resolve(result);
+            link = response.headers.link && response.headers.link.match(/(?<=<).*?(?=>)/g)[0];
+            results = results.concat(response.body.results)
+        } while (link);
+
+        redisClient.setCache('four_square', hash, ttl, results, (err, data) => {
+            if (err || !data) {
+                console.error(err);
+                return reject('Error setting redis cache');
+            }
+            return resolve(results);
         });
     });
 }
--- foursquare-api-place-search.js
+++ foursquare-api-place-search.js
@@ -4,28 +4,13 @@
 require('dotenv').config();
 const query = require('./caching/api-query');
 
-const TARGET_CHAIN_ID = 'ab4b8230-d68a-012e-5619-003048cad9da';
-
 (async () => {
-    const data = {};
-    const stores = [
-        'Target'
-    ];
-    const cities = [
-        {name: 'Houston, TX', region: 'US.TX.RR'},
-    ]
-    for (let metroCity of cities) {
-        const {name: cityName} = metroCity;
-        data[cityName] = [];
-        for (let store of stores) {
-            const request = {
-                near: cityName,
-                chains: TARGET_CHAIN_ID,
-                limit: 50
-            };
-            const results = await query(request, 1000 * 60 * 60);
-            console.log(results);
-        }
-    }
+    const request = {
+        near: 'Houston, TX',
+        chains: 'ab4b4810-d68a-012e-5619-003048cad9da',
+        limit: 50
+    };
+    const results = await query(request, 1000 * 60);
+    console.log(results.length);
     process.exit()
 })()
